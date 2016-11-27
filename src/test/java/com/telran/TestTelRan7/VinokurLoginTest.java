7ed812d2a31ac73e25f8f18528e2e9d1bf99af38
vinokur <vinokur.n@gmail.com>
2016-11-27 22:11:35 +0200

    Commit Login Test

---
+++ src/test/java/com/telran/TestTelRan7/VinokurLoginTest.java
@@ -0,0 +1,11 @@
+package com.telran.TestTelRan7;
+
+import com.telran.TestNgTestBase;
+
+/**
+ * Created by user on 27.11.2016.
+ */
+public class VinokurLoginTest extends TestNgTestBase{
+
+    public void VinokurLoginPage vinokurLoginPage ()
+}
